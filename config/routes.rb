1eb9aada5d323c23bf118263631afe046dbd2af7
hettoritosi <your.email@example.com>
2017-01-19 17:50:46 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get  '/help',   to: 'static_pages#help'
   
   get  'static_pages/about'
+  
+  get  '/signup',  to: 'users#new'
 
   resources :microposts
   resources :users
